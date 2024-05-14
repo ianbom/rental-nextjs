35e194b676d5b3ecd1556558d07fd1fcf7600df5
ApriliaCristy <ianaleale123@gmail.com>
2024-05-15 02:01:58 +0700

    yey bisa

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { GoAdmin } from "@/kendaraan_components/button";
 
 export default function Home() {
   return (
     <>
     <h1>Welcome To HomePage</h1>
         <h1>Langsung tembak route aja /admin</h1>
+        <GoAdmin/>
     </>
     
   )
--- kendaraan_components/button.tsx
+++ kendaraan_components/button.tsx
@@ -95,3 +95,14 @@ export const SubmitCustomer = ({ Label }: { Label: string }) => {
 };
 
 
+export const GoAdmin = () => {
+    return (
+        <Link
+            href="/admin"
+            className="inline-flex items-center space-x-1 text-white bg-blue-700 hover:bg-blue-800 px-5 py-[9px] rounded-sm text-sm">
+
+            <IoAddSharp size={20} />
+            Go Admin
+        </Link>
+    )
+}
